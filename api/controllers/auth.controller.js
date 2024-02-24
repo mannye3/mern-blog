034ec7b454cb463d7d659a18afe2903e2daae8fc
manny3e <aboajahemmanuel@gmail.com>
2024-02-24 09:25:41 +0100

    Error Handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,31 +1,33 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res, next) => { 
-
-   
+export const signup = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
-        if (!username || !email ||!password || username === '' || password === '' || email === '')   {
-            return res.status(400).json({ error: "All fields are required" });
+        if (!username || !email || !password || username === '' || password === '' || email === '') {
+            return next(errorHandler(400, 'All fields are required'));
         }
 
-        const hashedPassword = bcryptjs.hashSync(password, 10)
+        const hashedPassword = bcryptjs.hashSync(password, 10);
         const user = await User.findOne({ email });
         const userName = await User.findOne({ username });
         if (user) {
-            return res.status(400).json({ error: "User already exists" });
+             return next(errorHandler(400, 'User already exists'));
+            
         }
 
-         if (userName) {
-            return res.status(400).json({ error: "Username taken" });
+        if (userName) {
+             return next(errorHandler(400, 'Username taken'));
+           
         }
+
         const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
         res.status(201).json({ message: "User created successfully" });
     } catch (error) {
         next(error);
     }
-}
+};
 
    
