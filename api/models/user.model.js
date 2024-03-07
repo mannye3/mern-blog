e5b0a57603851453053db81bfeb655111e08965a
manny3e <aboajahemmanuel@gmail.com>
2024-03-07 08:30:01 +0100

    Add Admin

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema({
         type: String,
        default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw3pGhL4-mRIcZZQlxUNTeys&ust=1709197991598000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCICg0dzYzYQDFQAAAAAdAAAAABAE',
     },
+
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
 }, {timestamps: true});
 
 const User = mongoose.model("User", userSchema);
